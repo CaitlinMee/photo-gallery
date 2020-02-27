c1efb11bc4ddd4ae65cf69946dec5ff88c2dcb9d
CaitlinMee <57130231+CaitlinMee@users.noreply.github.com>
2020-02-27 19:12:34 +1100

    Added Filesystem API

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -10,27 +10,71 @@ import { Plugins, CameraResultType, Capacitor,
   providedIn: 'root'
 })
 
-
-
 export class PhotoService {
   public photos: Photo[] = [];
 
   constructor() { }
 
+  private async savePicture(cameraPhoto: CameraPhoto) {
+    // Convert photo to base64 format, required by Filesystem API to save
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+    // Write the file to the data directory
+    const fileName = new Date().getTime() + '.jpeg';
+    await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+    });
+
+    // Get platform-specific photo filepaths
+    return await this.getPhotoFile(cameraPhoto, fileName);
+  }
+
+  private async readAsBase64(cameraPhoto: CameraPhoto) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+
+  private async getPhotoFile(cameraPhoto: CameraPhoto, 
+    fileName:string): Promise<Photo> {
+      return {
+        filepath: fileName,
+        webviewPath: cameraPhoto.webPath
+      };
+    }
+
   public async addNewToGallery() {
     // take a photo
     const capturedPhoto = await Camera.getPhoto ({
-      resultType: CameraResultType.Uri,
-      source: CameraSource.Camera,
-      quality: 100
+      resultType: CameraResultType.Uri,  // file-based data; provides best performance
+      source: CameraSource.Camera,  // automatically take a new photo with the camera
+      quality: 100  // highest quality (0 to 100)
     });
 
     this.photos.unshift({
       filepath: "soon...",
       webviewPath: capturedPhoto.webPath
     });
+
+    // Save the picture and add it to photo collection
+    const savedImageFile = await this.savePicture(capturedPhoto);
+      this.photos.unshift(savedImageFile);
+    }
   }
-}
+
 
 interface Photo {
   filepath: string;

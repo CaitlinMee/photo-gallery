414d45455a5b8451857b597e46c610a50ccb5b79
CaitlinMee <57130231+CaitlinMee@users.noreply.github.com>
2020-02-27 19:44:23 +1100

    Added Platform-specific Logic

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -33,16 +33,27 @@ export class PhotoService {
     });
 
     // Get platform-specific photo filepaths
-    return await this.getPhotoFile({ cameraPhoto, fileName });
+    return await this.getPhotoFile(cameraPhoto, fileName);
   }
 
   private async readAsBase64(cameraPhoto: CameraPhoto) {
+    // "hybrid" will detect Cordova or Capacitor
+    if (this.platform.is('hybrid')) {
+      //read the file into base64 format
+      const file = await Filesystem.readFile({
+        path: cameraPhoto.path
+      });
+
+      return file.data;
+    }
+    else {
     // Fetch the photo, read as a blob, then convert to base64 format
     const response = await fetch(cameraPhoto.webPath!);
     const blob = await response.blob();
 
     return await this.convertBlobToBase64(blob) as string;
   }
+}
 
   convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
     const reader = new FileReader;
@@ -53,28 +64,49 @@ export class PhotoService {
     reader.readAsDataURL(blob);
   });
 
-  private async getPhotoFile({ cameraPhoto, fileName }: { cameraPhoto: CameraPhoto; fileName: string; }): Promise<Photo> {
+  private async getPhotoFile(cameraPhoto, fileName) {
+    if (this.platform.is('hybrid')) {
+      // Get the new, complete filepath of the photo saved on filesystem
+      const fileUri = await Filesystem.getUri({
+        directory: FilesystemDirectory.Data,
+        path: fileName
+      });
+
+      // Display the new image by rewriting the 'file://' path to HTTP
+      // Details: https://ionicframework.com/docs/building/webview#file-protocol
+      return {
+        filepath: fileUri.uri,
+        webviewPath: Capacitor.convertFileSrc(fileUri.uri),
+      };
+    }
+    else {
+      // Use webPath to display the new image instead of base64 since it's
+      // already loaded into memory
       return {
         filepath: fileName,
         webviewPath: cameraPhoto.webPath
       };
     }
-
+  }
     public async loadSaved() {
       //retrieve cached photo array data
       const photos = await Storage.get({ key: this.PHOTO_STORAGE });
       this.photos = JSON.parse(photos.value) || [];
 
-      // Display the photo by reading into base64 format
-      for (let photo of this.photos) {
-        // Read each saved photo's from the Filesystem
-        const readFile = await Filesystem.readFile({
-          path: photo.filepath,
-          directory: FilesystemDirectory.Data
-        });
+      // Easiest way to detect when runnin on the web:
+      // "when the platform is NOT hybrid, do this"
+      if (!this.platform.is('hybrid')) {
+        // Display the photo by reading into base64 format
+        for (let photo of this.photos) {
+          // Read each saved photo's from the Filesystem
+          const readFile = await Filesystem.readFile({
+            path: photo.filepath,
+            directory: FilesystemDirectory.Data
+          });
 
-        // Web platform only: Save the photo into the base64 field
-        photo.base64 = `date:image/jpeg;base64,${readFile.data}`;
+          // Web platform only: Save the photo into the base64 field
+          photo.base64 = `date:image/jpeg;base64,${readFile.data}`;
+        }
       }
     }
 
@@ -97,7 +129,9 @@ export class PhotoService {
 
       Storage.set({
         key: this.PHOTO_STORAGE,
-        value: JSON.stringify(this.photos.map(p => {
+        value: this.platform.is('hybrid')
+          ? JSON.stringify(this.photos)
+          : JSON.stringify(this.photos.map(p => {
           // Don't save the base64 representation of the photo data,
           // since it's alreadt saved on the Filesyste,
           const photoCopy = { ...p };

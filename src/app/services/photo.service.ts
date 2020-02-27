90dcfba78b6b4626e514b6dfd5058003d12bac28
CaitlinMee <57130231+CaitlinMee@users.noreply.github.com>
2020-02-27 19:25:44 +1100

    Storage API

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -12,6 +12,7 @@ import { Plugins, CameraResultType, Capacitor,
 
 export class PhotoService {
   public photos: Photo[] = [];
+  private PHOTO_STORAGE: string = "photos";
 
   constructor() { }
 
@@ -28,7 +29,7 @@ export class PhotoService {
     });
 
     // Get platform-specific photo filepaths
-    return await this.getPhotoFile(cameraPhoto, fileName);
+    return await this.getPhotoFile({ cameraPhoto, fileName });
   }
 
   private async readAsBase64(cameraPhoto: CameraPhoto) {
@@ -48,36 +49,64 @@ export class PhotoService {
     reader.readAsDataURL(blob);
   });
 
-  private async getPhotoFile(cameraPhoto: CameraPhoto, 
-    fileName:string): Promise<Photo> {
+  private async getPhotoFile({ cameraPhoto, fileName }: { cameraPhoto: CameraPhoto; fileName: string; }): Promise<Photo> {
       return {
         filepath: fileName,
         webviewPath: cameraPhoto.webPath
       };
     }
 
-  public async addNewToGallery() {
-    // take a photo
-    const capturedPhoto = await Camera.getPhoto ({
-      resultType: CameraResultType.Uri,  // file-based data; provides best performance
-      source: CameraSource.Camera,  // automatically take a new photo with the camera
-      quality: 100  // highest quality (0 to 100)
-    });
+    public async loadSaved() {
+      //retrieve cached photo array data
+      const photos = await Storage.get({ key: this.PHOTO_STORAGE });
+      this.photos = JSON.parse(photos.value) || [];
 
-    this.photos.unshift({
-      filepath: "soon...",
-      webviewPath: capturedPhoto.webPath
-    });
+      // Display the photo by reading into base64 format
+      for (let photo of this.photos) {
+        // Read each saved photo's from the Filesystem
+        const readFile = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: FilesystemDirectory.Data
+        });
 
-    // Save the picture and add it to photo collection
-    const savedImageFile = await this.savePicture(capturedPhoto);
-      this.photos.unshift(savedImageFile);
+        // Web platform only: Save the photo into the base64 field
+        photo.base64 = `date:image/jpeg;base64,${readFile.data}`;
+      }
     }
-  }
 
+    public async addNewToGallery() {
+      // take a photo
+      const capturedPhoto = await Camera.getPhoto ({
+        resultType: CameraResultType.Uri,  // file-based data; provides best performance
+        source: CameraSource.Camera,  // automatically take a new photo with the camera
+        quality: 100  // highest quality (0 to 100)
+      });
 
-interface Photo {
-  filepath: string;
-  webviewPath: string;
-  base64?: string;
-}
+      this.photos.unshift({
+        filepath: "soon...",
+        webviewPath: capturedPhoto.webPath
+      });
+
+      // Save the picture and add it to photo collection
+      const savedImageFile = await this.savePicture(capturedPhoto);
+        this.photos.unshift(savedImageFile);
+
+      Storage.set({
+        key: this.PHOTO_STORAGE,
+        value: JSON.stringify(this.photos.map(p => {
+          // Don't save the base64 representation of the photo data,
+          // since it's alreadt saved on the Filesyste,
+          const photoCopy = { ...p };
+          delete photoCopy.base64;
+
+          return photoCopy;
+          }))
+        });
+      }
+    }
+
+      interface Photo {
+        filepath: string;
+        webviewPath: string;
+        base64?: string;
+      }
